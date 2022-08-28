149e1744f75f54c36f64282ee5db973b99f5a139
softcoder404 <softcoder404@gmail.com>
2022-08-28 12:15:03 +0100

    first commit

--- lib/src/constants/api_keys.dart
+++ lib/src/constants/api_keys.dart
@@ -1,6 +1,8 @@
 import 'package:todo/src/constants/key.dart';
 
 class ApiKeys {
-  static const String BASE_URL = AppKeys.isDebug ? "" : "";
+  static const String BASE_URL = AppKeys.isDebug
+      ? "https://www.domain-stagging.com/v1/api/"
+      : "https://www.domain-production.com/v1/api/";
   static const String LOGIN_ENDPOINT = "/login";
 }
